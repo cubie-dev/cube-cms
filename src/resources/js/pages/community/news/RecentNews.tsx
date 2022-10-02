40a57e8f159064de0166c9b8657963f3efd0a580
Eran Machiels <dev@eranmachiels.nl>
2022-10-02 11:23:51 +0200

    Removed types array to automatically include @types/* (#17)

--- src/resources/js/pages/community/news/RecentNews.tsx
+++ src/resources/js/pages/community/news/RecentNews.tsx
@@ -5,6 +5,7 @@ import { IResponse } from '../../../interfaces/IResponse';
 import { List, ListItem } from '../../../components/shared/list';
 import { useIntl } from 'react-intl';
 import { localeFormat } from '../../../support/localeFormat';
+import route from 'ziggy-js';
 
 export const RecentNews = () => {
     // null is not loaded yet
--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -11,5 +12,10 @@ export default defineConfig({
     ],
     build: {
         target: 'es2019'
+    },
+    resolve: {
+        alias: {
+            'ziggy-js' : path.resolve(__dirname, 'vendor/tightenco/ziggy/src/js/index.js'),
+        },
     }
 })
